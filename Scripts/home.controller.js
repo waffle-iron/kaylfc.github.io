8aa1f4198f9fa34dc7bbc5f96b07629e85a4d7b4
Sam Kay <samkay@Sams-MBP.home>
2015-09-20 18:56:44 +0100

    Add Angular

---
+++ Scripts/home.controller.js
@@ -0,0 +1,25 @@
+angular
+    .module('app')
+    .controller('HomeController', HomeController);
+
+HomeController.$inject = ['countries'];
+
+function HomeController(countries) { 
+	var vm = this;
+	
+	vm.area = "World";
+	vm.stateChanged = false;
+	vm.hovering = false;
+	
+	var countriesData = countries.get();
+
+	////////////////////////////
+
+	vm.changeArea = function() {
+		var rand = Math.floor(Math.random() * countriesData.length);
+		var key = Object.keys(countriesData[rand].name.native)[0];
+
+		vm.area = countriesData[rand].name.native[key].common;
+		vm.stateChanged = !vm.stateChanged;
+	}
+}
